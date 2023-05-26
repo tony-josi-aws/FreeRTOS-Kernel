2d2c1526f972f58897d807465193aa23d1b7edc6
tony-josi-aws <tony-josi-aws@users.noreply.github.com>
2023-05-26 07:34:43 +0000

    [AUTO][RELEASE]: Bump task.h version macros to "250.4.4"

--- include/task.h
+++ include/task.h
@@ -53,8 +53,8 @@
  * The tskKERNEL_VERSION_MAJOR, tskKERNEL_VERSION_MINOR, tskKERNEL_VERSION_BUILD
  * values will reflect the last released version number.
  */
-#define tskKERNEL_VERSION_NUMBER       "V10.4.4+"
-#define tskKERNEL_VERSION_MAJOR        10
+#define tskKERNEL_VERSION_NUMBER       "V250.4.4"
+#define tskKERNEL_VERSION_MAJOR        250
 #define tskKERNEL_VERSION_MINOR        4
 #define tskKERNEL_VERSION_BUILD        4
 
